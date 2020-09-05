ca9b5baa250ebf6bd35369f98adc7a4c6a612516
kim344 <dongju8907@gmail.com>
2020-09-05 21:43:27 +0900

    Day19 Commit

---
+++ app/src/main/java/com/kim344/kotlinbasicsample/Day19Activity.kt
@@ -0,0 +1,45 @@
+package com.kim344.kotlinbasicsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Day19Activity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_none)
+
+        twoAndThree{a,b -> a+b}
+        twoAndThree{a,b -> a*b}
+
+        val ex1 : (String) -> Unit = { str : String ->
+            println("$str ex1 람다함수")
+        }
+
+        val ex2 : (String) -> Unit = { str : String ->
+            println("$str ex2 람다함수")
+        }
+
+        val ex3 : (String) -> String = { str : String ->
+            println("$str ex3 람다함수")
+            "return Message"
+        }
+
+        higherFunc(ex1)
+        higherFunc(ex2)
+        returnString(ex3)
+    }
+
+    private fun twoAndThree(operation : (Int, Int) -> Int){
+        val result = operation(1,2)
+        println("result $result")
+    }
+
+    private fun higherFunc( hf : (String) -> Unit){
+        hf("higherFunc 호출")
+    }
+
+    private fun returnString( rs : (String) -> String){
+        val rtStr = rs("returnString 호출")
+        println(rtStr)
+    }
+}
